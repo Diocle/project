4e0bb88db4102966fd7ee5ebbfa8cf3ace599ffb
Dmitry <example@gmail.com>
2022-04-11 17:31:59 +0300

    intertemp

--- interpolizia.js
+++ interpolizia.js
@@ -3,3 +3,6 @@ const category = 'toys';
 
 // console.log("https://someurl.com/" + category); непрактичный метод
    console.log(`https://someurl.com/${category}/5`);
+   
+const user = "Ivan";
+alert(`Hello, ${user}`);
